20ea9dddad5a10b2d6e47cdd8d48015d4fbd27bd
yafeunteun <yannfeunteun@gmail.com>
2018-05-26 21:30:44 +0200

    feat: can pass user as a parameter instead of hardcoding it.

--- main.py
+++ main.py
@@ -1,64 +1,75 @@
+import click
+
 import requests
 from bs4 import BeautifulSoup
+        
+@click.command()
+@click.option('--user', required=True,
+              help='The account to scrap (all photos and all captions).')
+def scrap(user):
+    """ Scrap photos and captions from posts of a single user """
+    BASE_URL = 'https://deskgram.org'
+    USER = user
 
-BASE_URL = 'https://deskgram.org'
-USER = 'trex'
+    start_url = BASE_URL + '/' + USER
 
-start_url = BASE_URL + '/' + USER
+    img_url_all = list()
+    caption_all = list()
 
-img_url_all = list()
-caption_all = list()
+    r = requests.get(start_url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    captions = soup.findAll("div", {"class": "post-caption"})
+    images = soup.findAll("div", {"class": "post-img"})
 
-r = requests.get(start_url)
-soup = BeautifulSoup(r.text, 'html.parser')
-captions = soup.findAll("div", {"class": "post-caption"})
-images = soup.findAll("div", {"class": "post-img"})
+    for image in images:
+        img_url = image.img['src'].split('?')[0]
+        img_url_all.append(img_url)
 
-for image in images:
-    img_url = image.img['src'].split('?')[0]
-    img_url_all.append(img_url)
+    for caption in captions:
+        caption_all.append(caption.text)
 
-for caption in captions:
-    caption_all.append(caption.text)
+    print ('Found {0} captions.'.format(len(captions)))
+    print ('Found {0} images.'.format(len(images)))
 
-print ('Found {0} captions.'.format(len(captions)))
-print ('Found {0} images.'.format(len(images)))
+    while True:
+        links = soup.findAll('a')
+        next_link = list(filter( lambda x: 'next_id' in x['href'], links))
+        if len(next_link) == 0:
+            break
+        else:
+            dest = next_link[0]['href']
+            next_url = BASE_URL + dest
+            print ('fetching {0}'.format(next_url))
+            r = requests.get(next_url)
+            soup = BeautifulSoup(r.text, 'html.parser')
+            captions = soup.findAll("div", {"class": "post-caption"})
+            images = soup.findAll("div", {"class": "post-img"})
 
-while True:
-    links = soup.findAll('a')
-    next_link = list(filter( lambda x: 'next_id' in x['href'], links))
-    if len(next_link) == 0:
-        break
-    else:
-        dest = next_link[0]['href']
-        next_url = BASE_URL + dest
-        print ('fetching {0}'.format(next_url))
-        r = requests.get(next_url)
-        soup = BeautifulSoup(r.text, 'html.parser')
-        captions = soup.findAll("div", {"class": "post-caption"})
-        images = soup.findAll("div", {"class": "post-img"})
+            for image in images:
+                img_url = image.img['src'].split('?')[0]
+                img_url_all.append(img_url)
 
-        for image in images:
-            img_url = image.img['src'].split('?')[0]
-            img_url_all.append(img_url)
-
-        for caption in captions:
-            caption_all.append(caption.text)
+            for caption in captions:
+                caption_all.append(caption.text)
 
 
-        print ('Found {0} captions.'.format(len(captions)))
-        print ('Found {0} images.'.format(len(images)))
+            print ('Found {0} captions.'.format(len(captions)))
+            print ('Found {0} images.'.format(len(images)))
 
-for idx, img_url in enumerate(img_url_all):
-    img_data = requests.get(img_url).content
-    filename = '{0}_{1}.jpg'.format(USER, idx)
-    print('saving image {0}'.format(filename))
-    with open(filename, 'wb') as handler:
-        handler.write(img_data)
+    for idx, img_url in enumerate(img_url_all):
+        img_data = requests.get(img_url).content
+        filename = '{0}_{1}.jpg'.format(USER, idx)
+        print('saving image {0}'.format(filename))
+        with open(filename, 'wb') as handler:
+            handler.write(img_data)
      
-for idx, caption in enumerate(caption_all):
-    filename = '{0}_{1}.txt'.format(USER, idx)
-    print('saving caption {0}'.format(filename))
-    text_file = open(filename, 'w')
-    text_file.write(caption)
-    text_file.close()
+    for idx, caption in enumerate(caption_all):
+        filename = '{0}_{1}.txt'.format(USER, idx)
+        print('saving caption {0}'.format(filename))
+        text_file = open(filename, 'w')
+        text_file.write(caption)
+        text_file.close()
+
+
+if __name__ == '__main__':
+    scrap()
